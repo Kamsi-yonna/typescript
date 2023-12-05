8dff6b45bc068469fd9ffb76eb7e6ca3d048f456
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-12-05 19:11:34 +0100

    Update sandbox.ts

--- sandbox.ts
+++ sandbox.ts
@@ -17,22 +17,44 @@
 // console.log(circ(3));
 
 // arrays
-var names = ["kam", "see", "yonna"];
+// var names = ["kam", "see", "yonna"];
 
-names.push("red");
-names.push(23);
-names[0] = "change";
-console.log(names);
+// names.push("red");
+// names.push(23);
+// names[0] = "change";
+// console.log(names);
 
-var numbers = [34, 54, 23];
+// var numbers = [34, 54, 23];
 
-numbers.push(232);
-numbers[0] = 99;
-console.log(numbers);
+// numbers.push(232);
+// numbers[0] = 99;
+// console.log(numbers);
 
-var mixed = ["red", 34, "second"];
+// var mixed = ["red", 34, "second"];
 
-mixed.push(9000);
-mixed.push("newone");
-mixed[0] = 34;
-console.log(mixed);
+// mixed.push(9000);
+// mixed.push("newone");
+// mixed[0] = 34;
+// console.log(mixed);
+
+//EXPLOICIT TYPES
+// SETTING THE TYPES
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// age = "red";
+age = 32;
+
+// isLoggedIn = 34
+isLoggedIn = true;
+
+// foor arrays
+let devs: string[];
+// let devs: string[] = []; initializes the devs array to an empty array
+
+devs = ["red", "reddd", "freddy"];
+// devs = [34, 23, 4332]; error
+devs.push("blue");
+
+console.log(devs);
