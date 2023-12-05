da4f96ccceab6fe3de8b721c700ca8f29342d46a
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-12-05 18:45:04 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,29 @@
-var character = "real";
-console.log(character);
-var inputs = document.querySelectorAll("input");
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
-character = "red";
-var myName = "kamsiyonna";
-myName = "30";
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(3));
+// var character = "real";
+// console.log(character);
+// var inputs = document.querySelectorAll("input");
+// console.log(inputs);
+// inputs.forEach(function (input) {
+//   console.log(input);
+// });
+// character = "red";
+// var myName = "kamsiyonna";
+// myName = "30";
+// var circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(circ(3));
+// arrays
+var names = ["kam", "see", "yonna"];
+names.push("red");
+names.push(23);
+names[0] = "change";
+console.log(names);
+var numbers = [34, 54, 23];
+numbers.push(232);
+numbers[0] = 99;
+console.log(numbers);
+var mixed = ["red", 34, "second"];
+mixed.push(9000);
+mixed.push("newone");
+mixed[0] = 34;
+console.log(mixed);
