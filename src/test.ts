eb20b5e2b20efcb53aa088111c149bd6f06c239c
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-12-06 18:22:08 +0100

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,42 @@
 "strict mode";
-console.log("rest");
+// console.log("rest");
+// let greet = () => {
+//   console.log("hello world");
+// };
+
+// greet();
+
+// let rest: Function;
+// // rest = red;
+// // rest = 34;
+// rest = () => {
+//   console.log("resting red");
+// };
+// rest();
+
+// // * Optional Parameters
+
+// const add = (a: number, b: number, c?: string | number) => {
+//   console.log(a + b);
+// };
+
+// add(4, 5);
+
+// const minus = (a: number, b: number) => {
+//   return a - b;
+// };
+
+// let result = minus(5, 2);
+// console.log(result);
+
+// * ALLIAS
+const logDetails = (uid: string | number, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+};
+logDetails("kamsy", "red");
+
+const greet = (user: { uid: string | number; string: string }) => {
+  console.log(`${user.name} says hi `);
+};
+
+greet();
