bf13ebc3972facb71a3cced1f751e1b85286b511
Kamsiyonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-12-06 12:22:57 +0100

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,4 @@
+"use strict";
 // var character = "real";
 // console.log(character);
 // var inputs = document.querySelectorAll("input");
@@ -5,82 +6,59 @@
 // inputs.forEach(function (input) {
 //   console.log(input);
 // });
-
 // character = "red";
 // var myName = "kamsiyonna";
-
 // myName = "30";
 // var circ = (diameter: number) => {
 //   return diameter * Math.PI;
 // };
-
 // console.log(circ(3));
-
 // arrays
 // var names = ["kam", "see", "yonna"];
-
 // names.push("red");
 // names.push(23);
 // names[0] = "change";
 // console.log(names);
-
 // var numbers = [34, 54, 23];
-
 // numbers.push(232);
 // numbers[0] = 99;
 // console.log(numbers);
-
 // var mixed = ["red", 34, "second"];
-
 // mixed.push(9000);
 // mixed.push("newone");
 // mixed[0] = 34;
 // console.log(mixed);
-
 //EXPLOICIT TYPES
 // SETTING THE TYPES
 // let character: string;
 // let age: number;
 // let isLoggedIn: boolean;
-
 // // age = "red";
 // age = 32;
-
 // // isLoggedIn = 34
 // isLoggedIn = true;
-
 // // foor arrays
 // let devs: string[];
 // // let devs: string[] = []; initializes the devs array to an empty array
-
 // devs = ["red", "reddd", "freddy"];
 // // devs = [34, 23, 4332]; error
 // devs.push("blue");
-
 // console.log(devs);
-
 // // UNION TYPES
 // let mixed: (string | number)[] = [];
-
 // // mixed.push(true); booleans arent accepted
 // mixed.push(32);
 // mixed.push("westttt");
-
 // console.log(mixed);
-
 // let uid: string | number;
-
 // // objects
 // let ninjaOne: object;
 // ninjaOne = { name: "rexy", age: 34 };
-
 // console.log(ninjaOne);
-
 // let ninjaTwo: {
 //   name: string;
 //   age: number;
 //   isBelt: boolean;
 // };
-
 // ADDING THE TSCONFIG.JSON FILE
 console.log("test");
